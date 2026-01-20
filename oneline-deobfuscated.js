b3386cfd4442ceefcbbc32bf5b523e600f923706
Leonard Lesinski <oxygen@leox.dev>
2026-01-20 15:19:17 +0100

    Create oneline-deobfuscated.js

---
+++ oneline-deobfuscated.js
@@ -0,0 +1,17 @@
+(function () {
+  return ([selfFunction, variables] = [
+    (otherFunction, loopOptions) =>
+      [
+        selfFunction,
+        (function () {
+          for (
+            let i = 0;
+            loopOptions?.c?.(i) ?? i < 1;
+            loopOptions?.p?.() ?? i++
+          )
+            otherFunction(variables, i);
+        })(),
+      ][0],
+    {},
+  ])[0];
+})();
